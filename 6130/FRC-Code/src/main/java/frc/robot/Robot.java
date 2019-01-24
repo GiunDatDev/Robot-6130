6ff292a4ccb6e817c7c41121916d69f803a83e4f
Will Nguyen <40159926+ShaunMuffin@users.noreply.github.com>
2019-01-24 14:41:07 -0500

    Add files via upload

--- 6130/FRC-Code/src/main/java/frc/robot/Robot.java
+++ 6130/FRC-Code/src/main/java/frc/robot/Robot.java
@@ -7,11 +7,21 @@
 
 package frc.robot;
 
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 
 /**
@@ -28,17 +38,20 @@ public class Robot extends TimedRobot {
   private static final int masterRightPort = 2;
   private static final int slaveRightPort = 3;
   private static final int controllerPort = 0;
-  private static final int pneumaticControllerPort = 1;
-  
-  // Define a deathzone for the joystick
-  private static final double joystickDeathZone = 0.3;
+  private static final int compressorPort = 0;
+
   // Define a zone to activate pneumatic
-  private static final double pneumaticZone = 0.5;
+  private static final double joystickDeathZone = 0.3;
 
 
   private SpeedController masterLeftMotor, slaveLeftMotor, masterRightMotor, slaveRightMotor;
-  private Joystick driverController;
-  private Joystick pneumaticController;
+  private XboxController driverController;
+  private DifferentialDrive driveTrain;
+  private SpeedControllerGroup leftDrive;
+  private SpeedControllerGroup rightDrive;
+  private Compressor compressor;
+  private DoubleSolenoid firstSolenoid, secondSolenoid;
+
 
   @Override
   public void robotInit() {
@@ -46,9 +59,32 @@ public class Robot extends TimedRobot {
     slaveLeftMotor = new PWMVictorSPX(slaveLeftPort);
     masterRightMotor = new PWMVictorSPX(masterRightPort);
     slaveRightMotor = new PWMVictorSPX(slaveRightPort);
-    driverController = new Joystick(controllerPort);
-    pneumaticController = new Joystick(pneumaticControllerPort);
-  }
+    driverController = new XboxController(controllerPort);
+    leftDrive =  new SpeedControllerGroup(masterLeftMotor, slaveLeftMotor);
+    rightDrive = new SpeedControllerGroup(masterRightMotor, slaveRightMotor);
+    driveTrain = new DifferentialDrive(leftDrive, rightDrive);
+    compressor = new Compressor(compressorPort);
+    firstSolenoid = new DoubleSolenoid(0, 2);
+    secondSolenoid = new DoubleSolenoid(1, 3);
+
+    // This code here is used to control the camera or the robot vision
+    new Thread(() -> {
+      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+      camera.setResolution(1366, 768);
+      
+      CvSink cvSink = CameraServer.getInstance().getVideo();
+      CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+      
+      Mat source = new Mat();
+      Mat output = new Mat();
+      
+      while(!Thread.interrupted()) {
+          cvSink.grabFrame(source);
+          Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+          outputStream.putFrame(output);
+      }
+  }).start();
+}
 
   // Running forward or backward depended on the vertical value of the joystick
   // Running left or right depended on the horizontal value of the joystick
@@ -57,47 +93,7 @@ public class Robot extends TimedRobot {
   // If the robot turns right, the left side will run forward and the right side will run backward
   // Changing the direction of the robot to left or right need to be done first
   // Running forward and backward will be done after the robot changes it horizontal direction
-  private void verticalDrive(double controllerVal){
-    masterLeftMotor.set(-controllerVal);
-    slaveLeftMotor.set(-controllerVal);
-    masterRightMotor.set(controllerVal);
-    slaveRightMotor.set(controllerVal);
-  }
-
-  private void horizontalDrive(double controllerVal){
-    masterLeftMotor.set(controllerVal);
-    slaveLeftMotor.set(controllerVal);
-    masterRightMotor.set(controllerVal);
-    slaveRightMotor.set(controllerVal);
-  }
-
-  private boolean checkJoystickZone(double controllerVal){
-      if (controllerVal < -joystickDeathZone || controllerVal > joystickDeathZone){
-        return true;
-      }
-      else {
-        return false;
-    }
-  }
-
-  // This function will check if the user want to change the horizontal direction of the robot or not
-  // If it is true, the robot will change the direction first
-  // Otherwise, the robot will continue to move in vertical direction
-  private void driveRobot(){
-    double controllerXValue = driverController.getX();
-    double controllerYValue = driverController.getY();
-
-   while (checkJoystickZone(controllerXValue) == true){
-     horizontalDrive(controllerXValue);
-   }
-   if (checkJoystickZone(controllerYValue)){
-     verticalDrive(controllerYValue);
-   }
-  }
-
   // This code from here will control the pneumatic system
-  DoubleSolenoid firstSolenoid = new DoubleSolenoid(0, 2);
-  DoubleSolenoid secondSolenoid = new DoubleSolenoid(1, 3);
 
   private void forwardPiston(){
     firstSolenoid.set(DoubleSolenoid.Value.kForward);
@@ -109,33 +105,46 @@ public class Robot extends TimedRobot {
     secondSolenoid.set(DoubleSolenoid.Value.kReverse);
   }
 
-  private void stopPiston(){
-    firstSolenoid.set(DoubleSolenoid.Value.kOff);
-    secondSolenoid.set(DoubleSolenoid.Value.kOff);
-  }
-
-  private boolean pneumaticCheckZone(double zoneValue){
-    if (zoneValue >= pneumaticZone){
+  private boolean checkJoystickZone(double controllerVal){
+    if (controllerVal < -joystickDeathZone || controllerVal > joystickDeathZone){
       return true;
     }
     else {
       return false;
-    }
   }
-
-  public void pistonController(){
-      double pneumaticYValue = pneumaticController.getY();
-      if (pneumaticCheckZone(pneumaticYValue) == true){
-        forwardPiston();
-      }
-      else {
-        reversePiston();
-      }
-  }
+}
 
   @Override
   public void teleopPeriodic() {
-    driveRobot();
-    pistonController();
+    // This code is used to control robot driving
+    if (checkJoystickZone(driverController.getRawAxis(1))){
+      driveTrain.tankDrive(-driverController.getRawAxis(1), -driverController.getRawAxis(1));
+    }
+    if (checkJoystickZone(driverController.getRawAxis(4))){
+      driveTrain.tankDrive(driverController.getRawAxis(4), -driverController.getRawAxis(4));
+    }
+
+    // This code here is used to control pneumatic system
+    if (driverController.getAButtonPressed()){
+      forwardPiston();
+    }
+
+    if (driverController.getAButtonReleased()){
+      reversePiston();
+    }
+
+    // This code here is used to control the air compressor
+    if (driverController.getBButtonPressed()){
+     compressor.start();
+     compressor.setClosedLoopControl(true);
+    }
+
+    if (driverController.getBButtonReleased()){
+      compressor.stop();
+      compressor.setClosedLoopControl(false);
+    }
   }
 }
+
+// We are missing the code for controlling the motor of the hatch catching
+// We are missing the code for controlling the other two motors of the ball mechanism
